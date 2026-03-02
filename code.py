8d0dcf01e648d6cfada639e6535860733e7a8572
stackerleee <cinthio@gmail.com>
2026-03-02 15:18:02 +0100

    Update code.py

--- code.py
+++ code.py
@@ -13,49 +13,72 @@ import digitalio
 from adafruit_hid.keyboard import Keyboard
 from adafruit_hid.keycode import Keycode
 
-# --- INITIALISERING ---
-# Vänta 10 sekunder för att låta NUC:en boota färdigt
+# --- 1. STARTFÖRDRÖJNING ---
+# Vänta 10 sekunder för att låta Linux/Blocks ladda drivrutiner
 time.sleep(10)
 
-# Setup för brytarna på GP18 och GP19
-btnswitch1 = digitalio.DigitalInOut(board.GP18)
-btnswitch1.direction = digitalio.Direction.INPUT
-btnswitch1.pull = digitalio.Pull.UP
+# --- 2. LÄS DIN keys.conf ---
+def get_config():
+    try:
+        with open("keys.conf", "r") as f:
+            lines = f.readlines()
+            # Rad 1: Pinnar (t.ex. board.GP18,board.GP19)
+            pins_str = lines[0].strip().split(',')
+            # Rad 2: Keycodes (t.ex. Keycode.KEYPAD_ONE,Keycode.KEYPAD_ZERO)
+            keys_str = lines[1].strip().split(',')
+            
+            # Mappa strängar till faktiska objekt
+            pins = [getattr(board, p.split('.')[-1]) for p in pins_str]
+            codes = [getattr(Keycode, k.split('.')[-1]) for k in keys_str]
+            return pins, codes
+    except Exception as e:
+        print(f"Kunde inte läsa keys.conf: {e}")
+        # Standardvärden om filen saknas
+        return [board.GP18, board.GP19], [Keycode.KEYPAD_ONE, Keycode.KEYPAD_ZERO]
 
-btnswitch2 = digitalio.DigitalInOut(board.GP19)
-btnswitch2.direction = digitalio.Direction.INPUT
-btnswitch2.pull = digitalio.Pull.UP
+target_pins, target_keys = get_config()
 
-prev_state1 = btnswitch1.value
-prev_state2 = btnswitch2.value
+# --- 3. SETUP AV PINNAR ---
+btn1 = digitalio.DigitalInOut(target_pins[0])
+btn1.direction = digitalio.Direction.INPUT
+btn1.pull = digitalio.Pull.UP
 
-# --- HUVUDLOOP MED FELHANTERING ---
+btn2 = digitalio.DigitalInOut(target_pins[1])
+btn2.direction = digitalio.Direction.INPUT
+btn2.pull = digitalio.Pull.UP
+
+prev_state1 = btn1.value
+prev_state2 = btn2.value
+
+# --- 4. HUVUDLOOP MED FELHANTERING ---
 while True:
     try:
-        # Skapa tangentbordsobjektet
         kbd = Keyboard(usb_hid.devices)
         
         while True:
-            cur_state1 = btnswitch1.value
-            cur_state2 = btnswitch2.value
+            cur_state1 = btn1.value
+            cur_state2 = btn2.value
             
-            # Om något har ändrats på brytarna
             if cur_state1 != prev_state1 or cur_state2 != prev_state2:
+                # Logik för PLAY: Om en lur lyfts (True)
+                if (cur_state1 and not prev_state2) or (cur_state2 and not prev_state1):
+                    # Tryck på KEYPAD_ONE (från din conf)
+                    kbd.press(target_keys[0])
+                    time.sleep(0.1)
+                    kbd.release(target_keys[0])
                 
-                # Om båda lurarna läggs ner
-                if (not cur_state1) and (not cur_state2):
-                    kbd.release(Keycode.ONE)
-                
-                # Om en lur lyfts (och den andra inte redan var uppe)
-                elif (cur_state1 and not prev_state2) or (cur_state2 and not prev_state1):
-                    kbd.press(Keycode.ONE)
+                # Logik för STOP: Om båda lurarna är nere (False)
+                elif (not cur_state1) and (not cur_state2):
+                    # Tryck på KEYPAD_ZERO (från din conf)
+                    kbd.press(target_keys[1])
+                    time.sleep(0.1)
+                    kbd.release(target_keys[1])
 
                 prev_state1 = cur_state1
                 prev_state2 = cur_state2
             
-            # Debounce (50ms) för att undvika fladder
-            time.sleep(0.05)
+            time.sleep(0.05) # Debounce
 
     except Exception as e:
-        # Om USB-kommunikationen bryts, vänta 5 sek och försök igen
+        # Om USB-anslutningen svajar, vänta 5 sek och försök igen
         time.sleep(5)
