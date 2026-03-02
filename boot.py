381f20fabcd42b10b88f64c12d5ff05c041cd9b7
stackerleee <cinthio@gmail.com>
2026-03-02 15:17:28 +0100

    Update boot.py

--- boot.py
+++ boot.py
@@ -1,11 +1,10 @@
-# boot.py
 import usb_hid
 import storage
 import usb_cdc
 
-# Inaktivera USB-minnet (CIRCUITPY-drivenheten)
+# Inaktivera USB-minnet (CIRCUITPY) så att Linux inte blir förvirrat
 storage.disable()
-# Inaktivera den seriella konsolen
+# Inaktivera seriell port
 usb_cdc.disable()
 # Aktivera endast tangentbordet
 usb_hid.enable((usb_hid.Device.KEYBOARD,))
