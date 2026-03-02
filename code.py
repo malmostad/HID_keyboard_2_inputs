2d752e165c6991399fdb4888ee6b7b361707e619
stackerleee <cinthio@gmail.com>
2026-03-02 15:15:39 +0100

    Update code.py

--- code.py
+++ code.py
@@ -10,34 +10,52 @@ import time
 import board
 import usb_hid
 import digitalio
-from adafruit_hid import keyboard
-from digitalio import DigitalInOut, Direction, Pull
+from adafruit_hid.keyboard import Keyboard
+from adafruit_hid.keycode import Keycode
 
-#  Pinout below for Pi Pico
+# --- INITIALISERING ---
+# Vänta 10 sekunder för att låta NUC:en boota färdigt
+time.sleep(10)
+
+# Setup för brytarna på GP18 och GP19
 btnswitch1 = digitalio.DigitalInOut(board.GP18)
 btnswitch1.direction = digitalio.Direction.INPUT
 btnswitch1.pull = digitalio.Pull.UP
-prev_state1 = btnswitch1.value
 
 btnswitch2 = digitalio.DigitalInOut(board.GP19)
 btnswitch2.direction = digitalio.Direction.INPUT
 btnswitch2.pull = digitalio.Pull.UP
+
+prev_state1 = btnswitch1.value
 prev_state2 = btnswitch2.value
 
-kbd = keyboard.Keyboard(usb_hid.devices)
-
+# --- HUVUDLOOP MED FELHANTERING ---
 while True:
-    cur_state1 = btnswitch1.value
-    cur_state2 = btnswitch2.value
-    if cur_state1 != prev_state1 or cur_state2 != prev_state2:
-        if (not cur_state1) and (not cur_state2):
-            # if both headphones are down, stop playing
-            print("Send keycode 1, Stop")
-            kbd.release(keyboard.Keycode.ONE)
-            # start playing if one of the headphones are picked up (and none were picked up before)
-        elif (cur_state1 and not prev_state2) or (cur_state2 and not prev_state1):
-            print("Send keycode 1, Play")
-            kbd.press(keyboard.Keycode.ONE)
+    try:
+        # Skapa tangentbordsobjektet
+        kbd = Keyboard(usb_hid.devices)
+        
+        while True:
+            cur_state1 = btnswitch1.value
+            cur_state2 = btnswitch2.value
+            
+            # Om något har ändrats på brytarna
+            if cur_state1 != prev_state1 or cur_state2 != prev_state2:
+                
+                # Om båda lurarna läggs ner
+                if (not cur_state1) and (not cur_state2):
+                    kbd.release(Keycode.ONE)
+                
+                # Om en lur lyfts (och den andra inte redan var uppe)
+                elif (cur_state1 and not prev_state2) or (cur_state2 and not prev_state1):
+                    kbd.press(Keycode.ONE)
 
-    prev_state1 = cur_state1
-    prev_state2 = cur_state2
+                prev_state1 = cur_state1
+                prev_state2 = cur_state2
+            
+            # Debounce (50ms) för att undvika fladder
+            time.sleep(0.05)
+
+    except Exception as e:
+        # Om USB-kommunikationen bryts, vänta 5 sek och försök igen
+        time.sleep(5)
